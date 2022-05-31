08ec56e84b6ac47d17640838b637dbe031a5bc94
eder.rodrigues <ederpr@gmail.com>
2022-05-31 06:55:13 -0300

    Classe de configuracao do Kafka

--- src/main/java/com/study/kafka/shopvalidator/events/KafkaConfig.java
+++ src/main/java/com/study/kafka/shopvalidator/events/KafkaConfig.java
@@ -1,2 +1,51 @@
-package com.study.kafka.shopvalidator.events;public class KafkaConfig {
+package com.study.kafka.shopvalidator.events;
+
+import com.study.kafka.shopvalidator.dto.ShopDto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KafkaConfig {
+    @Value(value = "${kafka.bootstrap.Address:172.21.45.40:9092}")
+    private String bootStrapAddress;
+    private Map<String, Object> properties;
+
+    public ProducerFactory<String, ShopDto> producerFactory(){
+        properties = new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapAddress);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "shop-api");
+
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, ShopDto> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    public ConsumerFactory<String, ShopDto> consumerFactory(){
+        JsonDeserializer<ShopDto> deserializer = new JsonDeserializer<>(ShopDto.class);
+        properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapAddress);
+        return new DefaultKafkaConsumerFactory<>(properties, new StringDeserializer(), deserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ShopDto> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, ShopDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 }
