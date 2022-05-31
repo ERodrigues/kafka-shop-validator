17aa66551e11b41c19c5a03b67f293bad643170f
eder.rodrigues <ederpr@gmail.com>
2022-05-31 06:56:42 -0300

    repositorio para busca de um produto pelo seu atributo identifier

--- src/main/java/com/study/kafka/shopvalidator/repository/ProductRepository.java
+++ src/main/java/com/study/kafka/shopvalidator/repository/ProductRepository.java
@@ -1,2 +1,8 @@
-package com.study.kafka.shopvalidator.repository;public interface ProductRepository {
+package com.study.kafka.shopvalidator.repository;
+
+import com.study.kafka.shopvalidator.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByIdentifier(String identifier);
 }
