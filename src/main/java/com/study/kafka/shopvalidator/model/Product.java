e99668b57cbe7d80b7f90cb42bb2caea129080ae
eder.rodrigues <ederpr@gmail.com>
2022-05-31 06:55:56 -0300

    Entidade que validara se o produto existe ou nao

--- src/main/java/com/study/kafka/shopvalidator/model/Product.java
+++ src/main/java/com/study/kafka/shopvalidator/model/Product.java
@@ -1,2 +1,18 @@
-package com.study.kafka.shopvalidator.model;public class Product {
+package com.study.kafka.shopvalidator.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="product_identifier")
+    private String identifier;
+    private Integer amount;
 }
