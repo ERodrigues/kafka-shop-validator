cee6511290ed69d8c49fb9441b6a209f8e98738f
eder.rodrigues <ederpr@gmail.com>
2022-06-06 05:57:06 -0300

    Adicao de configuracoes da aplicacao consumidora

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into shop.product (product_identifier, amount) values ('123456789', 100);
-insert into shop.product (product_identifier, amount) values ('987654321', 200);
+insert into product (product_identifier, amount) values ('123456789', 1000);
+insert into product (product_identifier, amount) values ('987654321', 2000);
